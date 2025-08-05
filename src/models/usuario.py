bc68ba16fb07cc1d3738b2cc1dc07cc3ff625995
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2025-08-05 11:02:02 -0600

    En esta carpeta se modelan todas las tablas a generar, leer, actualizar o eliminar de la base de datos

---
+++ src/models/usuario.py
@@ -0,0 +1,7 @@
+from src.management_db.db import db
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    correo = db.Column(db.String(120), unique=True, nullable=False)
