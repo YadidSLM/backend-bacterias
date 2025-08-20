41c6cdaa30703f91f0754b10ffa2736113031fac
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2025-08-20 14:30:38 -0600

    Se añadió una restricción a la tabla nodo, que es una única combinación entre gen y su módulo para que no se puedan insertar los mismos datos en caso que se ejecute load_data.py más de una vez

--- src/models/nodo.py
+++ src/models/nodo.py
@@ -7,6 +7,10 @@ class Nodo(db.Model):
     id_gen = db.Column(db.Integer, db.ForeignKey('gen.id_gen'), nullable=False)
     id_coexp_modulo = db.Column(db.Integer, db.ForeignKey('coexp_modulo.id_coexp_modulo'), nullable="False")
 
+    __table_args__ = (
+        db.UniqueConstraint('id_gen', 'id_coexp_modulo', name = 'unico_gen_modulo'),
+    )
+
     gen = db.relationship('Gen', back_populates='nodo')
     modulo = db.relationship('Coexp_modulo', back_populates='nodo')
     arista_from = db.relationship('Arista', foreign_keys='Arista.id_from_node', back_populates='from_node') #El atributo de la clase Nodo es arista y con ella se puede accede a la arista que contiene el nodo.
