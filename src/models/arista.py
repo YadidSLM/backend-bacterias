d6206bca7286b85dd9bf3cc8d25ada3eb43143ff
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2025-08-27 19:26:20 -0600

    Se añadió método __repr__() para imprimir en el blueprint el objeto

--- src/models/arista.py
+++ src/models/arista.py
@@ -8,7 +8,10 @@ class Arista(db.Model):
     id_to_node = db.Column(db.Integer, db.ForeignKey('nodo.id_nodo'), nullable=False)
     weight = db.Column(db.Float)
 
-    #El atributo de la clase Arista es from_node que permite acceder a from_node de la tabla Nodo.
+    #El atributo de la clase Arista es from_node que permite acceder a from_node de la tabla Nodo. En objetos python
     from_node = db.relationship('Nodo', foreign_keys=[id_from_node], back_populates='arista_from')
     to_node = db.relationship('Nodo', foreign_keys=[id_to_node], back_populates='arista_to')
+
+    def __repr__(self):
+        return f"<Arista id_arista = {self.id_arista}>, id_from_node = {self.id_from_node}, id_to_node = {self.id_to_node}, weight = {self.weight}"
 
