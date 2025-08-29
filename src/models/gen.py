8a6390f2b58e35d492241d0101644e7a43cf1bb0
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2025-08-29 16:35:51 -0600

    Se añade a la tabla gen una columna id_bacteria que dice la bacteria a la que le pertenece ese gen y también se añaden las relaciones entre objetos para acceder a gen desde bacteria y viceversa

--- src/models/gen.py
+++ src/models/gen.py
@@ -4,9 +4,11 @@ class Gen(db.Model):
     __tablename__ = 'gen'
     id_gen = db.Column(db.Integer, primary_key=True, autoincrement=True)
     locus_tag = db.Column(db.String(20), unique = True, nullable=False)
+    id_bacteria = db.Column(db.Integer, db.ForeignKey('bacteria.id_bacteria'), nullable=True)
 
     nodo = db.relationship('Nodo', back_populates='gen')
     expresiones = db.relationship('Expresion', back_populates='gen')
+    bacteria = db.relationship('Bacteria', back_populates='gen')
 
     def __repr__(self): #Para cuando se quiere imprimir el objeto. Es una función que ves su representación.
-        return print(f"id_gen: {self.id} \nlocus_tag: {self.locus_tag}")
+        return print(f"id_gen: {self.id}, locus_tag: {self.locus_tag}, id_bacteria: {self.id_bacteria}")
