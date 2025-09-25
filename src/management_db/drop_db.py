9bdfd3c60c8c254a24ed0b5e4d5a6665c8ab1f28
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2025-09-25 15:10:19 -0600

    Se modificaron cometarios

--- src/management_db/drop_db.py
+++ src/management_db/drop_db.py
@@ -1,7 +1,6 @@
 from src.app import app
 from src.management_db.db import db
 #Modelos de las tablas que se borran.
-# from src.models.modelos import Gene, Condition, Sample, Expression, CoexpressionModule, TranscriptionFactor, GeneTF, Node, Edge
 from src.models import Gen, Nodo, Arista, Coexp_modulo, Expresion
 
 with app.app_context():
