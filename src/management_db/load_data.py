db6dbe4fba1a822342b2fd399d8b9b577c29915e
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2025-09-25 15:09:11 -0600

    Nuevas modificaciones para añadir más bacterias, se lograron cargar Ypest, Paeu y se está cargando Ecoli

--- src/management_db/load_data.py
+++ src/management_db/load_data.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import os
-from src.management_db.db import db #Corre desde la terminal en backend-bacterias con python -m src.management_db.load_data
+from src.management_db.db import db #Corre este script desde la terminal en backend-bacterias con python -m src.management_db.load_data como se indica en README.md
 from src.app import app
 from src.models import Gen, Nodo, Arista, Coexp_modulo, Expresion, Bacteria
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
-file_path_colombos = os.path.join(base_dir, '..', 'archivos', 'Ypest', 'colombosypest.txt')
-file_path_modulo = os.path.join(base_dir, '..', 'archivos', 'Ypest', 'SignedCytoscapeInput-nodes-ypest-Bicor00.txt')
-file_path_aristas = os.path.join(base_dir, '..', 'archivos', 'Ypest', 'Signed-CytoscapeInput-edges-ypest-Bicor00.txt')
+file_path_colombos = os.path.join(base_dir, '..', 'archivos', 'Ecoli', 'colombosEcoli.txt')
+file_path_modulo = os.path.join(base_dir, '..', 'archivos', 'Ecoli', 'SignedCytoscapeInput-nodes-Ecoli-Bicor140.txt')
+file_path_aristas = os.path.join(base_dir, '..', 'archivos', 'Ecoli', 'Signed-CytoscapeInput-edges-Ecoli-Bicor140.txt')
 #Signed-CytoscapeInput-edges-lt2.txt
 
 
@@ -19,6 +19,9 @@ modulo = []
 locusTag = []
 expresion = []
 nodosList =[]
+
+print("Columnas de nodeModule:", nodeModule.columns)
+
 #Cargar bacterias
 """
 id_bacteria  bacteria
@@ -40,7 +43,7 @@ id_bacteria  bacteria
 16  Bcere
 17  Banth
 """
-bacterias = ["Ypest", "Tther", "Spneu", "Smeli", "Sflex", "Sente", "Paeru", "Mtube", "Lrham", "Hpylo", "Ecoli", "Cjeju", "Cacet", "Bthet", "Bsubt", "Bcere", "Banth"]#Lista de bacterias
+bacterias = ["Ypest", "Tther", "Spneu", "Smeli", "Sflex", "Sente", "Paeru", "Mtube", "Lrham", "Hpylo", "Ecoli", "Cjeju", "Cacet", "Bthet", "Bsubt", "Bcere", "Banth"]#Lista de bacterias, el índice de esta lista es el id_bacteria - 1
 with app.app_context():
     print("Entro a la app bacterias")
     try: #Si no hay registros en la base de datos
@@ -62,11 +65,14 @@ for row in nodeModule['nodeAttr[nodesPresent, ]']: #Cargar modulos en una lista 
         modulo.append(row)
 with app.app_context():
     print("Entro a la app módulos")
+    
     try: #Si no hay registros en la base de datos
         for color in modulo:
-            new_modulo = Coexp_modulo(nombre_modulo=color)
-            db.session.add(new_modulo)
-            db.session.commit()
+            existente = db.session.query(Coexp_modulo).filter_by(nombre_modulo=color).one_or_none()
+            if not existente:
+                new_modulo = Coexp_modulo(nombre_modulo=color)
+                db.session.add(new_modulo)
+                db.session.commit()
         else: #Al terminar el for
             print("Módulos registrados en la DB")
     except:
@@ -78,20 +84,28 @@ with app.app_context():
 
 
 #Cargar genes
+indiceBacteria = 10 #Índice de la bacteria que se quiere cargar, por ejemplo, para Mtube es 7, ojo que el índice es 7 aunque el id_bacteria es 8.
 for row in colombosBacteria['LocusTag']:
     if not(row in locusTag): #Asegura que no hay valores repetidos de locus tag
         locusTag.append(row)
 with app.app_context():
     print("Entro a la app genes")
-    #bact = db.session.query(Bacteria).filter(Bacteria.bacteria == bacteria[0]).one_or_none() #Por ahora todos son de Ypest, id_bacteria=1, bacterias[0] es Ypest
-    bact = db.session.query(Bacteria).filter(Bacteria.bacteria == bacterias[0]).one_or_none() #Por ahora todos son de Ypest, id_bacteria=1, bacterias[0] es Ypest
+    bact = db.session.query(Bacteria).filter(Bacteria.bacteria == bacterias[indiceBacteria]).one_or_none() #Insertar el índice de la bacteria que se quiera cargar aquí, por ejemplo, para Mtube es bacterias[7], ojo que el índice es 7 aunque el id_bacteria es 8.
     print(f"bacteria: {bact.bacteria}, id_bacteria: {bact.id_bacteria}")
+
     try:
         if bact:
             for gen in locusTag:
                 new_locus_tag = Gen(locus_tag=gen, id_bacteria=bact.id_bacteria) #Se obtiene el id_bacteria de la consulta anterior
-                db.session.add(new_locus_tag)
-                db.session.commit()
+                existente = db.session.query(Gen).filter_by(locus_tag=gen, id_bacteria=bact.id_bacteria).one_or_none()
+                if not existente:
+                    print("Se ingresa nuevo gen")
+                    db.session.add(new_locus_tag)
+                    db.session.commit()
+                else:
+                    #print(f"Ya existe el gen (locus_tag): {gen} en la base de datos")
+                    continue
+                
             else:
                 print("Genes registrados en la DB")
         else:
@@ -100,33 +114,15 @@ with app.app_context():
         db.session.rollback()
         locusTag.clear()
         print("Ya etsán los datos en la BD o Error al insertar genes:", str(e))
-    #raise SystemExit #Sale del programa si ya están los genes, pues las demás tablas dependen de estos.
-#Cargar expresiones a tabla expresión
-# print(colombosBacteria['2'].head(10))
-# for row in colombosBacteria['LocusTag']:
-#     print(f"{row}")
-"""
-Obtener los id_locus_tag de la base de datos, ponerlos en una
-lista para iterar la lista y con la variable que itere la lista
-index ponerla en colombosBacteria.loc[index], lugeo filtrar
-valores nulos e indicar cuáles son para saber si cambiarlos a cero o no
-dependiendo si la base de datos permite esos valores NaN y guardar la fila en
-otra lista de listas con el primer índice el locus tag y su contenido
-cada una de las muestras (fila extraida), por último iterar sobre ca
 
-El tipo de dato float sí acepta valores NaN como registros, los int no,
-Se saca cada fila de locus con sus muestras, se convierte a una lista,
-luego se itera en cada valor de la lista para tener las expresiones una por una,
-una vez que termiana la última muestra del locus, con el for que itera sobre todos los
-genes, cambia al que sigue y vuelve a hacer lo mismo, tarda en ejecutarse, pues
-su complejidad es n^2.
-"""
-
+#Cargar expresiones
 with app.app_context():
     print("Entro a la app expresion")
     try:
-        all_LocusTags = db.session.query(Gen.id_gen, Gen.locus_tag).all()
-        # locus_tag = 'PSLT099'
+        bact = db.session.query(Bacteria).filter(Bacteria.bacteria == bacterias[indiceBacteria]).one_or_none() #Insertar el índice de la bacteria que se quiera cargar aquí, por ejemplo, para Mtube es bacterias[7], ojo que el índice es 7 aunque el id_bacteria es 8.
+        all_LocusTags = db.session.query(Gen).filter(Gen.id_bacteria == bact.id_bacteria).all() #Hay que hacer restricción para que sólo saque los locus_tag de la bacteria que se está cargando, por ejemplo, para Mtube es id_bacteria=8
+        # Ej. locus_tag = 'PSLT099'
+        all_LocusTags = [(gen.id_gen, gen.locus_tag) for gen in all_LocusTags] #Lista de tuplas (id_gen, locus_tag)
         for id_locus_tag, locus_tag in all_LocusTags:
             print(f"{id_locus_tag}, {locus_tag}")
             locus_expr_dataFrame = colombosBacteria[colombosBacteria['LocusTag'].str.contains(locus_tag)] #Regresa la fila que se indica en contains .
@@ -142,20 +138,12 @@ with app.app_context():
                     new_expresion = Expresion(id_gen=id_locus_tag, id_muestra=id_sample, expresion=expr)
                     db.session.add(new_expresion)
                     db.session.commit()
-    except:
+    except Exception as e:
         db.session.rollback()
         all_LocusTags.clear()
-        print("Ya se tienen las expresiones de colombos.txt en la BD")
+        print("Ya se tienen las expresiones de colombos.txt en la BD, o Error al insertar expresiones:", str(e))
 
 #Cargar nodos
-"""
-Busca en la tabla cada nodeName con su respectivo módulo, pasa los datos de dataFrame a una lista de tuplas,
-descompone las tuplas en nodeName y module y obtiene su id_gen e id_módulo de la base de datos para verificar
-que ya se hayan ingresado esos genes y módulos y para obtener sus ids que se ingresan en la tabla nodo, luego
-inserta el id_gen en la columna de la tabla Nodo con su respectivo id_modulo, id_nodo se autoincrementa.
-En caso de que haya en la tabla .txt más nodos que no estén en colombos, guardarlos en una lista y
-pregntar a Edgardo qué hacer con esos. No fue el caso.
-"""
 nodosDataFrame = nodeModule.loc[0:, ["nodeName", "nodeAttr[nodesPresent, ]"]]
 nodosList = nodosDataFrame.values.tolist() #Lista de tuplas
 with app.app_context():
@@ -173,7 +161,7 @@ with app.app_context():
                 db.session.add(new_nodo)
                 db.session.commit()
             else:
-                print("No se obtuvieron datos requeridos")
+                print(f"No se obtuvieron datos requeridos {nodeName}, {module}")
                 break
         else:
             print("Nodos registrados en la BD")
@@ -181,6 +169,7 @@ with app.app_context():
         db.session.rollback()
         print("Ya se tienen estos nodos de nodes.txt en la BD")
 
+#Cargar aristas
 dfArista_FN_TN_W = aristasDataFrame.loc[0:, ["fromNode", "toNode", "weight"]] #df es dataframe
 edges_not_in_nodes = []
 with app.app_context():
@@ -193,42 +182,25 @@ with app.app_context():
             if fromNode_value and toNode_value:
                 existente = db.session.query(Arista).filter_by(id_from_node=fromNode_value.id_nodo, id_to_node=toNode_value.id_nodo, weight=peso).one_or_none()
                 if not existente:
-                    print("No existe la arista, se va a crear")
                     new_arista = Arista(id_from_node = fromNode_value.id_nodo, id_to_node = toNode_value.id_nodo, weight = peso) #fromNode_value es un objeto de modelado en models que tiene como atributo columnas y una de ellas es id_node
                     db.session.add(new_arista)
                     db.session.commit()
-                    print(f"{fromNode_value.id_gen}: {fromNode}, {toNode_value.id_gen}: {toNode}, weight: {peso}")
+                    print(f"Registrando {fromNode_value.id_gen}: {fromNode}, {toNode_value.id_gen}: {toNode}, weight: {peso}")
                 else:
                     print(f"Ya existe la arista: fromNode: {fromNode}, toNode: {toNode}, weight: {peso}")
+                    exit()
             else:
                 #Estos genes tienen baja correlación, por eso no están en nodos.
                 edges_not_in_nodes.append((fromNode, toNode, peso))
                 print(f"No se encuentran los locus_tag de la tabla edges.txt: fromNode: {fromNode}, toNode: {toNode} en la BD")
+                #print(nodeModule.head(10))
+                print(nodeModule[nodeModule['nodeName'] == "plum2"]) #Muestra si los locus_tag de las aristas que no se encuentran en la BD están en nodes.txt
+                exit()
+                
     
     except Exception as e:
         db.session.rollback()
         print("Ya están los datos en la BD o Error al insertar aristas:", str(e)) #El error es que no había nodos (de dfArista_FN_TN_W) en la BD nodo (y tampoco están en cyto..nodes.txt pues de esa tabla se llenó la tabla nodo), para que no cayera en el error se cambió de one() a one_or_none() para saber qué aristas intenta hacer la relación de la tabla edges.txt, pero no puede porque no están en nodos.
-        
-        # print("Ya se tienen registrados las aristas en la BD.")
-#Para Ypest, no hay nodos de las aristas que no estén en nodos, SMELI es una bacteria que le faltan datos en sus tablas.
-print(edges_not_in_nodes) #Estaba viendo las tablas y veo que sí se contempla una relación entre esos genes con un peso, pero no están en la de nodos con su módulo, ¿es cierto que no se incluyen en nodos aquellos genes que no tienen una relación tan fuerte?
-    
-# locus_tag = 'PSLT099'
-# # all_locus_expr = colombosBacteria.loc[colombosBacteria['LocusTag'] == id_locus_tag]
-# locus_expr = colombosBacteria[colombosBacteria['LocusTag'].str.contains(locus_tag)] #Regresa la columna que tiene el contains 
-# print(locus_expr.values.tolist())
-# for expre in locus_expr:
-# print(all_locus_expr.loc[id_locus_tag])
-    # print(expre)
-# print(type(locus_expr))
 
-# for id_muestra, val in enumerate(colombosBacteria.loc[colombosBacteria['LocusTag'] == id_locus_tag]): #.loc[0] devuelve la primera fila de la tabla y el for itera sobre cada valor de la fila.
-#     if type(val) is str:
-#         print(f"{id_muestra} : {val}") #Este es el locus tag
-        
-#     else:
-#         print(f"{id_locus_tag} : {id_muestra} : {float(val)}")
-
-# print(modulo)
-# print(locusTag)
-# print(len(locusTag))
+#Para Ypest, no hay nodos de las aristas que no estén en nodos (están completas las tablas .txt). SMELI es una bacteria que le faltan datos en sus tablas.
+print(edges_not_in_nodes) #Lista de tuplas (fromNode, toNode, weight) que no se encuentran en nodos. Se incluye esta lista por si le faltan datos a la tabla nodes.txt (como en el caso de SMELI) y se quieren agregar esos nodos faltantes a la tabla nodo de la BD.
